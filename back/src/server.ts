d58955930beb6935e003ca33330ffa75dbd3b1c4
OchaCoder <bygrd@tuta.io>
2025-05-02 05:05:55 +0900

    Backend --- Updated server CORS

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,5 +1,5 @@
 import Fastify from "fastify"
-// import fastifyCors from "@fastify/cors"
+import fastifyCors from "@fastify/cors"
 import { Type, TypeBoxTypeProvider } from "@fastify/type-provider-typebox"
 import { config } from "./config.js"
 
@@ -43,11 +43,11 @@ const fastify = Fastify({
   pluginTimeout: 6000, // default is 10000.
 }).withTypeProvider<TypeBoxTypeProvider>()
 
-// // Register CORS
-// fastify.register(fastifyCors, {
-//   origin: ["https://6813b9e7b3562700085de766--incomparable-marigold-3f81ff.netlify.app"],
-//   credentials: false, // only if cookies is used.
-// })
+// Register CORS
+fastify.register(fastifyCors, {
+  origin: ["https://6813b9e7b3562700085de766--incomparable-marigold-3f81ff.netlify.app"],
+  credentials: false, // only if cookies is used.
+})
 
 // Register Plugins
 fastify.register(errorHandlerPlugin)
@@ -135,7 +135,7 @@ const startServer = async () => {
     fastify.healthRedis("STARTUP")
 
     // Friendly start up log💚🌻
-    console.log("⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️")
+    console.log("🧹🧹🧹🧹🧹🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🧹🧹🧹🧹🧹")
     logStartupMessage(fastify)
 
     // Listen for Ctrl+C or system termination
