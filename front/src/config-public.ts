66d6710ad9539ca9a445cbf7e125b22f2d656339
OchaCoder <bygrd@tuta.io>
2025-05-02 03:08:23 +0900

    Frontend --- Updated config-public.ts

--- front/src/config-public.ts
+++ front/src/config-public.ts
@@ -1,13 +1,9 @@
 import { z } from "@builder.io/qwik-city"
 
 const envSchema = z.object({
-  FRONTEND_URL: z.string(),
   BACKEND_URL: z.string(),
-  ERRCATCHER_URL: z.string(),
 })
 
 export const configPublic = envSchema.parse({
-  FRONTEND_URL: import.meta.env.PUBLIC_FRONTEND_URL,
   BACKEND_URL: import.meta.env.PUBLIC_BACKEND_URL,
-  ERRCATCHER_URL: `${import.meta.env.PUBLIC_FRONTEND_URL}/errcatcher`,
 })
