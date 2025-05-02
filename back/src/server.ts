71b7f9f4ad42f5f6024d9b0c90b2cb565bdf22f9
OchaCoder <bygrd@tuta.io>
2025-05-02 09:00:56 +0900

    Backend --- Updated server.ts minimum set-up for test

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,14 @@
 import Fastify from "fastify"
-
+import fastifyCors from "@fastify/cors"
 import { config } from "./config.js"
 
 // Create fastify instance
 const fastify = Fastify()
+
+//Register CORS
+fastify.register(fastifyCors, {
+  origin: true, // only if cookies is used.
+})
 
 fastify.get("/test", (request, reply) => reply.status(200).send("The request is received!"))
 
@@ -13,7 +18,7 @@ const startServer = async () => {
     fastify.listen({ port: config.PORT })
     await fastify.ready()
 
-    console.log("Server is listening.")
+    console.log(`Fastify is listening on port ${config.PORT}`)
   } catch (err) {
     console.error("💥Server failed to start:", err)
     process.exit(1)
