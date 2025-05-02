4ed847d330210d86622e658e4ccd3fbcca6c54ee
OchaCoder <bygrd@tuta.io>
2025-05-02 10:18:01 +0900

    FE --- SIGN IN toast deleted

--- front/src/routes/sign-in/SignIn.tsx
+++ front/src/routes/sign-in/SignIn.tsx
@@ -26,7 +26,7 @@ export const SignIn = component$(() => {
     if (!backendHealth.stable) return
 
     // 3-2. Perform input check before enabling button click.
-    const { showToast, checkEmpty } = await onSubmitInputChecker(ctr, { email, password })
+    const { checkEmpty } = await onSubmitInputChecker(ctr, { email, password })
 
     // 3-3. No need to show toast. Inputs are only name and password.
     //showToast()
